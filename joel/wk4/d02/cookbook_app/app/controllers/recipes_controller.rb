dec9ec586095db48eea5d1cea13dba2936c2b908
Joel Hampton <joelhampton@gmail.com>
2014-12-02 11:55:41 +1100

    We did a bunch on stuff

---
+++ joel/wk4/d02/cookbook_app/app/controllers/recipes_controller.rb
@@ -0,0 +1,38 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+   @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @recipe = Recipe.find params[:id]
+  end
+
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :name,
+      :course,
+      :cooktime,
+      :servingsize,
+      :instructions,
+      :image,
+      :book_id
+    )
+  end
+
+end
