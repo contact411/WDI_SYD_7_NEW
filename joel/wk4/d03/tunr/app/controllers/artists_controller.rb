5dd4a4e9a57f65234975c8df9d7b2ee98a4b41c9
Joel Hampton <joelhampton@gmail.com>
2014-12-03 15:21:24 +1100

    Artist create working..

--- joel/wk4/d03/tunr/app/controllers/artists_controller.rb
+++ joel/wk4/d03/tunr/app/controllers/artists_controller.rb
@@ -1,7 +1,36 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :edit, :update, :destroy]
+
   def index
     @artists = Artist.all
   end
 
+  def new
+    @artist = Artist.new
+  end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @artist = Artist.new artist_params
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+
+private
+  def find_artist
+    @artist = Artist.find params[:id]
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :hometown, :bio, :label)
+  end
 end
