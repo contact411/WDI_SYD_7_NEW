52c5e9d478775abc4da8fd098521714ddc8648a0
Joel Hampton <joelhampton@gmail.com>
2014-12-16 12:55:59 +1100

    Created stuff.. So much fun.. Gun ===> Head

--- joel/wk6/d02/blog/spec/features/reader_lists_posts_spec.rb
+++ joel/wk6/d02/blog/spec/features/reader_lists_posts_spec.rb
@@ -5,4 +5,16 @@ feature "ReaderListsPosts", :type => :feature do
     visit '/' 
     expect(page).to have_content("No blog posts found")
   end
+
+  scenario "one blog post" do
+    Post.create(
+      title: "Hello World",
+      author: "Jane Birkin",
+      content: "Lorem ipsum dolar sit amet"
+      )
+    visit '/'
+    expect(page).to have_content("Hello World")
+    expect(page).to have_content("Jane Birkin")
+    expect(page).to have_content("Lorem ipsum dolar sit amet")
+  end
 end
