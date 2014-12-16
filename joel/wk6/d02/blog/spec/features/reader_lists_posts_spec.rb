127473e8e2b40087dc7be3f2f6fbc8d570c351a9
Joel Hampton <joelhampton@gmail.com>
2014-12-16 15:20:05 +1100

    Reader can list all blog posts.

--- joel/wk6/d02/blog/spec/features/reader_lists_posts_spec.rb
+++ joel/wk6/d02/blog/spec/features/reader_lists_posts_spec.rb
@@ -18,4 +18,22 @@ feature "ReaderListsPosts", :type => :feature do
     expect(page).to have_content("Lorem ipsum dolar sit amet")
     expect(page).not_to have_content("No blog posts found")
   end
+
+  scenario "multiple blog posts in system" do
+    (0..3).each do |n|
+      Post.create(
+          title: "Hello World #{n}",
+          author: "Author #{n}",
+          content: "Lorem ipsum #{n} dolar sit amet"
+        )
+      visit '/'
+    end
+
+    (0..3).each do |n|
+      expect(page).to have_content("Hello World #{n}")
+      expect(page).to have_content("Author #{n}")
+      expect(page).to have_content("Lorem ipsum #{n} dolar sit amet")
+      expect(page).not_to have_content("No blog posts found")
+    end
+  end
 end
